f710166bc41ddfaff07ee78b422524fe6971f94a
Vidhi Kamdar <vkamdar1@binghamton.edu>
2017-10-16 04:34:47 +0000

    MyLogger.java : Added switch cases for debugLevels 0, 1, 2, 3 and 4

--- airportSecurityState/src/airportSecurityState/util/MyLogger.java
+++ airportSecurityState/src/airportSecurityState/util/MyLogger.java
@@ -6,7 +6,7 @@ public class MyLogger{
       DEBUG_VALUE=3 [Print to stdout everytime the state is changed]
       DEBUG_VALUE=2 [FIXME: add your own scheme here]
       DEBUG_VALUE=1 [FIXME: add your own scheme here]
-      DEBUG_VALUE=0 [No output should be printed from the applicatio to stdout. It is ok to write to the output file though" ]
+      DEBUG_VALUE=0 [No output should be printed from the application to stdout. It is ok to write to the output file though" ]
     */
 
     public static enum DebugLevel {RELEASE, FROM_RESULTS, IN_RESULTS, IN_RUN, CONSTRUCTOR};
@@ -15,11 +15,18 @@ public class MyLogger{
 
     public static void setDebugValue (int levelIn){
 	switch (levelIn){
-	case 4: debugLevel = DebugLevel.CONSTRUCTOR;
-	    break;
+	
 	      // FIXME: add code for other cases
 	case 0: debugLevel = DebugLevel.RELEASE;
 	    break;
+	case 1: debugLevel = DebugLevel.FROM_RESULTS;
+	    break;
+	case 2: debugLevel = DebugLevel.IN_RESULTS;
+	    break;
+	case 3: debugLevel = DebugLevel.IN_RUN;
+	    break;
+	case 4: debugLevel = DebugLevel.CONSTRUCTOR;
+	    break;	
 	}
     }
 
